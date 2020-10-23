bfb75d59da2f932d0213df938fb0dca882c4588a
Bart Janssens <bart@bartjanssens.org>
2020-10-24 01:34:24 +0200

    Try dlopen

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using TestQtTravis
+using Libdl
 
-run(`otool -L /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5/Qt3DAnimation`)
+l = "/Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5/Qt3DAnimation"
+run(`otool -L $l`)
+@show dlopen(l)
 
 # @show TestQtTravis.Qt_jll.libqt53danimation_path
 # run(`otool -L $(TestQtTravis.Qt_jll.libqt53danimation_path)`)
