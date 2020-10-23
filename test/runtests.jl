cc76f81aabc2a99a86214d9096a5a06108633272
Bart Janssens <bart@bartjanssens.org>
2020-10-24 01:27:15 +0200

    Try otool again

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,6 @@
 using TestQtTravis
 
-run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/`)
-run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework`)
-run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions`)
-run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5`)
-run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5/Qt3DAnimation.dylib`)
-run(`otool -L /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5/Qt3DAnimation.dylib`)
+run(`otool -L /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5/Qt3DAnimation`)
 
 # @show TestQtTravis.Qt_jll.libqt53danimation_path
 # run(`otool -L $(TestQtTravis.Qt_jll.libqt53danimation_path)`)
