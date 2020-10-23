c46edd8f8063ddecdae17b4e7dfcfbd151a394fa
Bart Janssens <bart@bartjanssens.org>
2020-10-24 01:16:30 +0200

    and yet more

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using TestQtTravis
 
 run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/`)
 run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework`)
+run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions`)
+run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5`)
 run(`ls /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5/Qt3DAnimation.dylib`)
 run(`otool -L /Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5/Qt3DAnimation.dylib`)
 
