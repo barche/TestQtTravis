c3d6b809ac3ea2004991c09302f135dbcf89b23f
Bart Janssens <bart@bartjanssens.org>
2020-10-24 21:39:26 +0200

    List loaded libraries

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,5 @@
 using TestQtTravis
 using Libdl
-
-using DependencyWalker
-# using Qt_jll
-
-l = "/Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5/Qt3DAnimation"
-# l = Qt_jll.libqt53danimation_path
-
-deps = Library(l)
-@show deps
-display(deps)
-
 using Qt_jll
 
-# @show TestQtTravis.Qt_jll.libqt53danimation_path
-# run(`otool -L $(TestQtTravis.Qt_jll.libqt53danimation_path)`)
+display(dllist())
