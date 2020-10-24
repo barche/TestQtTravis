a84d7ae9ad2a1791a65f31484d03d78d54c35f23
Bart Janssens <bart@bartjanssens.org>
2020-10-24 15:00:57 +0200

    Try DependencyWalker

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,15 @@
 using TestQtTravis
 using Libdl
 
+using DependencyWalker
+# using Qt_jll
+
 l = "/Users/travis/.julia/artifacts/2cb154cd91aa24f8120a30441414a4507ad0311f/lib/Qt3DAnimation.framework/Versions/5/Qt3DAnimation"
-run(`otool -L $l`)
-@show dlopen(l)
+# l = Qt_jll.libqt53danimation_path
+
+deps = Library(l)
+@show deps
+display(deps)
 
 # @show TestQtTravis.Qt_jll.libqt53danimation_path
 # run(`otool -L $(TestQtTravis.Qt_jll.libqt53danimation_path)`)
